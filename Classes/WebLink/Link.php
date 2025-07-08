b4fc35fae112d3219126f27aca2ec45bce68a4a2
Dušan Krstić <krsticdusan2006@gmail.com>
2025-07-08 17:21:07 +0200

    izmena linka opet

--- Classes/WebLink/Link.php
+++ Classes/WebLink/Link.php
@@ -1,7 +1,7 @@
 <?php
 
 class Link{
-    private $webadress = "https://specificationsapp.netlify.app/";
+    private $webadress = "https://specificationsapp.netlify.app";
 
     public function getLink(){
         return $this->webadress;
