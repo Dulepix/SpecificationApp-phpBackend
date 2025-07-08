10129b73e080b14ebef25cb0e5c43d7455465977
Dušan Krstić <krsticdusan2006@gmail.com>
2025-07-08 19:09:23 +0200

    setupovanje baze

--- Classes/Connection.php
+++ Classes/Connection.php
@@ -5,14 +5,22 @@ class Connection {
     private $username;
     private $password;
     private $dbname;
+    private $port;
 
     protected function connect() {
-        $this->servername = "localhost";
-        $this->username = "root";
-        $this->password = "";
-        $this->dbname = "reactdb";
+        // $this->servername = "localhost";
+        // $this->username = "root";
+        // $this->password = "";
+        // $this->dbname = "reactdb";
 
-        $conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+        $this->servername = "biiox1gbnxtolzmbmbvl-mysql.services.clever-cloud.com";
+        $this->username = "ukaafovfut2sw1xv";
+        $this->password = "W8ZmlOsU8kLmOFjD7WtU";
+        $this->dbname = "biiox1gbnxtolzmbmbvl";
+
+        $this->port = 3306;
+
+        $conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname, $this->port);
         return $conn;
     }
 }
--- Classes/WebLink/Link.php
+++ Classes/WebLink/Link.php
@@ -1,7 +1,8 @@
 <?php
 
 class Link{
-    private $webadress = "https://specificationsapp.netlify.app";
+    // private $webadress = "https://specificationsapp.netlify.app";
+    private $webadress = "http://localhost:5173";
 
     public function getLink(){
         return $this->webadress;
